db06fad4deff36f38cff608bbd1ea314924ec609
daniellevin <daniil.levin@mail.huji.ac.il>
2020-01-19 00:35:23 +0200

    POST

--- app.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import json
-import requests
-
-from flask import Flask, request, make_response, jsonify, send_file
-
-print(10)
--- create_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE 'bibliography' (
-		'id'		        INTEGER PRIMARY KEY AUTOINCREMENT,
-		'abbreviation'	TEXT,
-		'description' 	TEXT NOT NULL, -- json or bibtex entry
-		'added_by'	    INTEGER NOT NULL
-);
-		
